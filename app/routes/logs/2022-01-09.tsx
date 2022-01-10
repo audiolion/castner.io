92a45fb64a7ad97e0293db7f1e73b8f218dfd156
Ryan Castner <castner.rr@gmail.com>
2022-01-09 22:05:53 -0500

    Jan 9 2022 log

---
+++ app/routes/logs/2022-01-09.tsx
@@ -0,0 +1,101 @@
+import logStyles from "../../styles/log.css";
+
+export function links() {
+  return [
+    {
+      rel: "stylesheet",
+      href: logStyles,
+    },
+  ];
+}
+
+export default function L2021_01_09() {
+  return (
+    <div className="log">
+      <h1>Daily Log</h1>
+      <h2>Jan 9, 2022</h2>
+      <ul>
+        <li>First day as a YTH Group Leader for my church!</li>
+        <li>
+          Two more chapters into{" "}
+          <a
+            href="https://www.amazon.com/Crucial-Conversations-Talking-Stakes-Second/dp/1469266822"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Crucial Conversations
+          </a>
+        </li>
+        <li>
+          Quarter of the way through
+          <a
+            href="https://www.learninpublic.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            The Coding Career Handbook
+          </a>{" "}
+          by{" "}
+          <a
+            href="https://www.swyx.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            swyx
+          </a>
+        </li>
+      </ul>
+      <p>
+        Sunday's are always very busy days for my family. My wife is a worship
+        director for our church, so that means our day starts early, 5:30am, and
+        we get back home around 12pm. I mix sound and make sure the broadcast
+        goes smoothly. This Sunday, I also started as a YTH group leader, it
+        pushes me out of my comfort zone working with teens as they build
+        relationships and learn about Christianity.
+      </p>
+      <p>
+        I did get a bit further in my books, and my next one arrived!{" "}
+        <a
+          href="https://staffeng.com/book"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Staff Engineer: Leadership beyond the management track
+        </a>{" "}
+        by Will Larson. It was an interesting choice I had with my free time, I
+        have been trying to finish{" "}
+        <a
+          href="https://www.youtube.com/watch?v=hkygiqC7ulQ"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Yakuza: Like a Dragon
+        </a>{" "}
+        for the better part of a year now, and Football was also on today. But
+        life is about choices and for now I am really focusing on my career and
+        my fitness. I made it to 10,000 steps today, it was the first day above
+        30° F and got outside for a nice long walk with Peach. And at YTH night
+        I got to play about 30 minutes of basketball. I have been playing{" "}
+        <a
+          href="https://pikminbloom.com/en/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Pikmin Bloom
+        </a>{" "}
+        to encourage me to walk more, with the goal of 10,000 a day. I also am
+        doing a 5-week bootcamp starting tomorrow through a local small business
+        that does personal training. This will be VoDs because I am trying to
+        minimize my social contact. I am somewhat concerned about long covid
+        symptoms. When I got mono at 20, it was pretty mild, but my energy was
+        gone for the better part of 6 months. And I was at my athletic peak,
+        working out daily and pursuing body building at the time. A similar
+        outcome would be crushing :(.
+      </p>
+      <p>
+        I have rambled on enough for now, I hear the puppy getting up to know
+        good, until tomorrow.
+      </p>
+    </div>
+  );
+}
